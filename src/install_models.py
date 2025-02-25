b450c02676ef71a46845b16da8f59697bd21ad88
royalsoftkv <38809329+royalsoftkv@users.noreply.github.com>
2025-02-25 14:51:53 +0100

    Add files via upload

---
+++ src/install_models.py
@@ -0,0 +1,15 @@
+from diffusers.models import AutoencoderKL
+from diffusers import AutoPipelineForText2Image
+import torch
+
+#model_name='SG161222/RealVisXL_V4.0'
+model_name='SG161222/RealVisXL_V5.0'
+
+vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix")
+pipe = AutoPipelineForText2Image.from_pretrained(
+    model_name,
+    vae=vae,
+    use_safetensors=True,
+    add_watermarker=False,
+    custom_pipeline="lpw_stable_diffusion_xl"
+)
