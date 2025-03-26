e5cb881187d8e4af46a413876f63d052182caed6
royalsoftkv <38809329+royalsoftkv@users.noreply.github.com>
2025-03-26 23:35:25 +0100

    Added clip model

--- src/install_models.py
+++ src/install_models.py
@@ -1,6 +1,7 @@
 from diffusers.models import AutoencoderKL
 from diffusers import AutoPipelineForText2Image
 import torch
+from transformers import CLIPProcessor, CLIPModel
 
 #model_name='SG161222/RealVisXL_V4.0'
 model_name='SG161222/RealVisXL_V5.0'
@@ -13,3 +14,7 @@ pipe = AutoPipelineForText2Image.from_pretrained(
     add_watermarker=False,
     custom_pipeline="lpw_stable_diffusion_xl"
 )
+
+clip_model_id = "openai/clip-vit-base-patch16"  # OpenAI CLIP model
+clip_model = CLIPModel.from_pretrained(clip_model_id)
+clip_processor = CLIPProcessor.from_pretrained(clip_model_id)
